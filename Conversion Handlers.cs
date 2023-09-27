841798dd6338702b121d4fa9b45e2f53847c4781
Astro <astroburgers1@gmail.com>
2023-09-26 20:32:11 -0500

    Fick jyeah boii

---
+++ Conversion Handlers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage.Native;
+
+namespace EUPtoUB
+{
+    internal static class Conversion_Handlers
+    {
+        internal static int GetDrawableID(PED_COMPONENT component)
+        {
+            return NativeFunction.Natives.x67F3780DD425D4FC<int>(EntryPoint.MainPlayer, (int)component) + 1; // GET_PED_DRAWABLE_VARIATION
+        }
+
+        internal static int GetTextureID(PED_COMPONENT component)
+        {
+            return NativeFunction.Natives.x04A355E041E004E6<int>(EntryPoint.MainPlayer, (int)component) + 1; // GET_PED_TEXTURE_VARIATION
+        }
+
+        internal static int GetPropID(PROP_TYPE prop)
+        {
+            return NativeFunction.Natives.x898CC20EA75BACD8<int>(EntryPoint.MainPlayer, (int)prop) + 1; // GET_PED_PROP_INDEX
+        }
+
+        internal static int GetPropTextureID(PROP_TYPE prop)
+        {
+            return NativeFunction.Natives.xE131A28626F81AB2<int>(EntryPoint.MainPlayer, (int)prop) + 1; // GET_PED_PROP_TEXTURE_INDEX
+        }
+    }
+}
--- Main.cs
+++ Main.cs
@@ -7,37 +7,62 @@ using Rage;
 using Rage.Attributes;
 using Rage.ConsoleCommands;
 using Rage.Native;
+using System.Windows.Forms;
 
 [assembly: Rage.Attributes.Plugin("EUP to UB", Description = "Does this even get read?", Author = "Astro")]
 
 namespace EUPtoUB
 {
+    internal enum PED_COMPONENT
+    {
+        PED_COMP_HEAD = 0,
+        PED_COMP_BERD = 1,
+        PED_COMP_HAIR = 2,
+        PED_COMP_TORSO = 3, // UPPERSKIN?
+        PED_COMP_LEG = 4, // PANTS
+        PED_COMP_HAND = 5, //PARACHUTE
+        PED_COMP_FEET = 6, // SHOES
+        PED_COMP_TEETH = 7, // ACCS?
+        PED_COMP_SPECIAL = 8, // UNDERCOAT?
+        PED_COMP_SPECIAL2 = 9, //PV_COMP_TASK 
+        PED_COMP_DECL = 10,      //DECAL
+        PED_COMP_JBIB = 11 // JBIB/SHIRT
+    }
+
+    internal enum PROP_TYPE
+    {
+        ANCHOR_HEAD = 0,
+        ANCHOR_EYES,
+        ANCHOR_EARS,
+        ANCHOR_MOUTH,
+        ANCHOR_LEFT_HAND,
+        ANCHOR_RIGHT_HAND,
+        ANCHOR_LEFT_WRIST,
+        ANCHOR_RIGHT_WRIST,
+        ANCHOR_HIP
+    }
+
     public class EntryPoint
     {
-        internal enum PED_COMPONENT
-        {
-            PED_COMP_HEAD = 0,
-            PED_COMP_BERD = 1,
-            PED_COMP_HAIR = 2,
-            PED_COMP_TORSO = 3,
-            PED_COMP_LEG = 4,
-            PED_COMP_HAND = 5,
-            PED_COMP_FEET = 6,
-            PED_COMP_TEETH = 7,
-            PED_COMP_SPECIAL = 8, //PV_COMP_ACCS
-            PED_COMP_SPECIAL2 = 9, //PV_COMP_TASK 
-            PED_COMP_DECL = 10,      //DECAL
-            PED_COMP_JBIB = 11 // JBIB
-        }
         internal static Ped MainPlayer => Game.LocalPlayer.Character;
         internal static void Main()
         {
+            FileHandler.ValidateFile();
+            while (true)
+            {
+                GameFiber.Yield();
 
+                if (Game.IsKeyDown(Keys.G))
+                {
+                    FileHandler.AppendFile();
+                }
+            }
         }
 
         internal static bool IsComponentValid(PED_COMPONENT component, int Drawable, int Texture)
         {
-            if (NativeFunction.Natives.IS_PED_COMPONENT_VARIATION_VALID<bool>(MainPlayer, component, Drawable, Texture))
+            // xE825F6B6CEA7671D = IS_PED_COMPONENT_VARIATION_VALID
+            if (NativeFunction.Natives.xE825F6B6CEA7671D<bool>(MainPlayer, component, Drawable, Texture))
             {
                 return true;
             }
