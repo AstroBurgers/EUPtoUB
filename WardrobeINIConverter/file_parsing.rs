f6f3cf5c50ca37da4ba488b432f6e402c73b3e99
AstroBurgers <astroburgers1@gmail.com>
2024-11-08 14:03:18 -0600

    america explen

---
+++ WardrobeINIConverter/file_parsing.rs
@@ -0,0 +1,32 @@
+﻿mod file_parsing{
+    use std::io;
+    use std::io::prelude::*;
+    use std::io::BufReader;
+    use std::fs::File;
+
+    fn parse_wardrobe() -> io::Result<()> {
+        let reader = BufReader::new(File::open("./plugins/EUP/wardrobe.ini")?).lines();
+
+
+    }
+
+    pub struct IniParser<R: Read>{
+        lines: Lines<BufReader<R>>,
+        kv: HashMap<String, String>
+    }
+
+    impl<R: Read> IniParser<R> {
+        pub fn parse(reader: R) -> IniParser<R> {
+            let mut parser = IniParser{
+                lines,
+                kv: HashMap::new()
+            };
+            
+            while let Some(Ok(line)) = parser.lines.next() {
+                parser.parse_line(&mut line.chars().peekable())?;
+            }
+
+            Ok(parser.kv)
+        }
+    }
+}
--- WardrobeINIConverter/io_handling/io_reading.rs
+++
@@ -1,5 +0,0 @@
-﻿use std;
-
-fn main() {
-
-}
