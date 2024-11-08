9e0a5abdc37ea6fce55d98a8495d8bd0dda45631
AstroBurgers <astroburgers1@gmail.com>
2024-11-08 13:45:14 -0600

    rust is weird

--- WardrobeINIConverter/src/main.rs
+++ WardrobeINIConverter/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome! Enter any letter to continue...");
+    press_any_key_to_continue();
+    println!("Continuing!")
+}
+
+fn press_any_key_to_continue() {
+    use std::io::{self, Read};
+    let _ = io::stdin().read(&mut [0u8]).unwrap();
 }
