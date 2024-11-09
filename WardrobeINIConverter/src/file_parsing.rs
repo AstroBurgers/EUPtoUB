a2e12914431d1876b53ba1f5e29be14230362e48
AstroBurgers <astroburgers1@gmail.com>
2024-11-08 22:54:17 -0600

    This is really weird no lie

--- WardrobeINIConverter/src/file_parsing.rs
+++ WardrobeINIConverter/src/file_parsing.rs
@@ -1,13 +1,34 @@
 ﻿mod file_parsing{
-    use nom;
+    use std::fs;
+    use std::io::{BufRead, BufReader, Lines};
+    use std;
+    use std::collections::HashMap;
+    use std::fs::File;
 
     struct IniRead{
         name: String,
-        comp_name: String,
-        data: Vec<i16, i16>
+        content: LineData,
     }
 
     impl IniRead {
+        pub fn start_parse(){
+            let file_path = "WardrobeINIConverter/plugins/EUP/wardrobe.ini";
+            let buff_reader = BufReader::new(fs::File::open(file_path)).lines()?;
+            for line in buff_reader{
+                let
+                let lineData = IniRead{
+                    name:
+                };
+            }
 
+
+        }
+
+
+    }
+
+    struct LineData{
+        comp_name: String,
+        data: HashMap<i16, i16>
     }
 }
