e37c2d7eab806ca370c8940e63b10fdd07f5cea1
AstroBurgers <astroburgers1@gmail.com>
2024-12-12 12:34:02 -0600

    Fuckk rust

---
+++ WardrobeINIConverter/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace WardrobeINIConverter;
+
+internal static class WardrobeIniConverter
+{
+    internal static void Main()
+    {
+        Console.WriteLine("Welcome!");
+        Console.WriteLine("Press any button to continue...");
+        
+    }
+}
